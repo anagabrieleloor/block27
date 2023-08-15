4508f5b4088c00c4b6bc2a2243206bcf235ecb3d
anagabrieleloor <anagabrieleloor@outlook.com>
2023-08-15 09:18:11 -0400

    added form validation

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,13 +5,29 @@ export default function SignUpForm({ setToken }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+    const [userData, setUserData] = useState(null);
+
+    //validation for username 
+    const usernameMinLength = 8;
+    const passwordMinLength = 6;
 
 
     async function handleSubmit(e) {
         e.preventDefault();
 
+
+        //validation
+        if (username.length < usernameMinLength) {
+            setError(`Username must be at least ${usernameMinLength} characters long.`);
+            return;
+        }
+
+        if (password.length < passwordMinLength) {
+            setError(`Password must be at least ${passwordMinLength} characters long.`);
+            return;
+        }
         console.log({ username, password });
-        
+
 
         try {
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
@@ -22,9 +38,12 @@ export default function SignUpForm({ setToken }) {
             console.log(result);
 
             setToken(result.token);
-
+            setError(null);
+            setUserData(result.user);
             //return result.token;
 
+            setUsername("");
+            setPassword("");
 
         } catch (error) {
             setError(error.message = "oh no!");
@@ -36,6 +55,9 @@ export default function SignUpForm({ setToken }) {
         <div>
             <h2>Sign Up!</h2>
             {username && <p>hi {username}</p>}
+            {userData && (
+                <p>yeeee success! welcome, {userData.username}!</p>
+            )}
             {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <label>
@@ -54,7 +76,9 @@ export default function SignUpForm({ setToken }) {
                 </label><br />
 
                 <button type="submit">Submit</button>
+                
             </form>
+            
         </div>
     )
 }
